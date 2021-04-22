99f47ee4951fd17739b9657ee90a175829a1acb3
Ben Kennedy <bmhk2011@gmail.com>
2021-04-22 10:43:16 -0600

    Setup complete

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Navbar } from 'react-bootstrap';
 
 class App extends React.Component {
-    render() {
-      return <h1>Hello, {this.props.name}</h1>;
-    }
+  constructor() {
+    super()
+    this.state = {}
   }
+
+  render() {
+    return (
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand>Kill Board</Navbar.Brand>
+      </Navbar>
+    )
+  }
+}
 
 export default App;
